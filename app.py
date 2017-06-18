b7b30dc5d150011b395e2bf6c64b3a5ffe180d4d
Markus <markus.maier3@gmx.at>
2017-06-18 21:05:03 +0200

    introducing 'single_currency'

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 #coding=utf-8
-from flask import Flask, url_for, session, request, render_template, jsonify
+from flask import Flask, url_for, request, render_template, jsonify 
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+from signal import signal, SIGPIPE, SIG_DFL
 from coinmarketcapAPI import *
 
 app = Flask(__name__)
+data = []
+@app.before_first_request
+def initialize():
+	load_data()
+	apsched = BackgroundScheduler()
+	apsched.start()
 
+	apsched.add_job(
+		func=load_data,
+		trigger=IntervalTrigger(seconds=5),
+		id="load_data_interval",
+		name="loading data from coinmarketcap",
+		replace_existing=True)
+def load_data():
+	global data
+	data = fetch_coin_data("bitcoin")
 @app.route("/")
 def index():
-	data = fetch_coin_data("bitcoin")
-	return render_template("index.html", data=data[0])
+	#data = fetch_coin_data("bitcoin")
+	return render_template("single_currency.html", data=data[0], currency="Bitcoin")
 
 @app.route("/<coinname>")
 def coin_data(coinname):
 	data = fetch_coin_data(coinname)
-	return jsonify(data)
-
+	#return jsonify(data)
+	return render_template("single_currency.html", data=data[0], currency=coinname)
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, threaded=True)
--- coinmarketcapAPI.py
+++ coinmarketcapAPI.py
@@ -1,15 +1,16 @@
 #coding=utf-8
-import urllib2, json
+import urllib2, json, time
 def fetch_coin_data(coin):
+	time_start = time.clock()
 	url = "https://api.coinmarketcap.com/v1/ticker/" + str(coin)
 	ua = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0"
-	head = {"User-agent": "de"}
+	head = {"User-agent": ua}
 	try:
 		request = urllib2.Request(url, None, head)
 		response =urllib2.urlopen(request).read()
 
 		jsonResponse = json.loads(response)
-
+		print "took: ", time.clock() - time_start, "ms"
 		return jsonResponse
 	except:
 		print "ERROR, couldnt fetch data with: ",coin
