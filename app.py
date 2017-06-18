82a8a794c46df54af652d8b3e218def8fe52b6b1
Markus <markus.maier3@gmx.at>
2017-06-18 21:54:43 +0200

    fetching data works now

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from signal import signal, SIGPIPE, SIG_DFL
 from coinmarketcapAPI import *
 
 app = Flask(__name__)
+
 data = []
+global_data = []
+
 @app.before_first_request
 def initialize():
 	load_data()
@@ -15,22 +18,31 @@ def initialize():
 
 	apsched.add_job(
 		func=load_data,
-		trigger=IntervalTrigger(seconds=5),
+		trigger=IntervalTrigger(seconds=10),
 		id="load_data_interval",
 		name="loading data from coinmarketcap",
 		replace_existing=True)
+
 def load_data():
-	global data
-	data = fetch_coin_data("bitcoin")
+	global global_data
+	global_data = fetch_coin_data("bitcoin")
+	print "reloaded data"
 @app.route("/")
 def index():
 	#data = fetch_coin_data("bitcoin")
-	return render_template("single_currency.html", data=data[0], currency="Bitcoin")
+	return render_template("single_currency.html", data=global_data[0], currency="Bitcoin")
 
 @app.route("/<coinname>")
 def coin_data(coinname):
 	data = fetch_coin_data(coinname)
 	#return jsonify(data)
 	return render_template("single_currency.html", data=data[0], currency=coinname)
+
+# /bitcoin makes a request to this and gets a json object as response with all the data
+@app.route("/getCoinData/<coinname>")
+def return_coin_data(coinname):
+	load_data()
+	return jsonify(global_data[0])
+
 if __name__ == "__main__":
 	app.run(debug=True, threaded=True)
--- coinmarketcapAPI.py
+++ coinmarketcapAPI.py
@@ -2,11 +2,12 @@
 import urllib2, json, time
 def fetch_coin_data(coin):
 	time_start = time.clock()
-	url = "https://api.coinmarketcap.com/v1/ticker/" + str(coin)
+	basic_url = "https://api.coinmarketcap.com/v1/ticker/"
+	url = basic_url + str(coin)
 	ua = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0"
 	head = {"User-agent": ua}
 	try:
-		request = urllib2.Request(url, None, head)
+		request = urllib2.Request(basic_url, None, head)
 		response =urllib2.urlopen(request).read()
 
 		jsonResponse = json.loads(response)
